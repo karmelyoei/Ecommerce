0be1848f1b29078a0b68c98d9a7a5349c042002f
karmelyoei <karmel.salah@gmail.com>
2020-10-22 01:20:40 +0300

    Establishing the shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shopepage.component'
+import { Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/hatspage/hatpage.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HatPage = ()=>{
+  return <h1>HatPage</h1>
+}
+
+export default HatPage
